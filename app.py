6b2cc3e08d6aa46b116aa90a0d60ed78c17d1445
Gouri Biju <164582227+Gouri-Biju@users.noreply.github.com>
2025-05-20 08:11:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,7 @@ if st.button("🔍 Predict Survival"):
         for i in range(100):
             time.sleep(0.079)
             progress.progress(i + 1)
+            time.sleep(0.02)
 
     prediction = model.predict(input_df)[0]
     result_text = "✅ **Survived**" if prediction == 1 else "❌ **Did not survive**"
